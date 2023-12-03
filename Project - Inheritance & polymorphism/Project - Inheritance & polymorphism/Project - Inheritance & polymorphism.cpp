46ea8c9f0188df40660b4ef27e39c2861702bf6c
josephsomogie <128629560+josephsomogie@users.noreply.github.com>
2023-12-03 14:37:35 -0500

    finished (mostly?)

--- Project - Inheritance & polymorphism/Project - Inheritance & polymorphism/Project - Inheritance & polymorphism.cpp
+++ Project - Inheritance & polymorphism/Project - Inheritance & polymorphism/Project - Inheritance & polymorphism.cpp
@@ -25,26 +25,31 @@ const string COLOR_RESET = "\033[0m";
 
 const string options[] = {
 		"0: Exit Program",
-		"1: Add Shape",
-		"2: Delete Shape",
-		"3: Clear List",
-		"4: Sort list",
-		"5: Search shapes",
-		"6: Display Shape",
-		"7: Demo"
+		"1: Test 2D",
+		"2: Test 3D",
+		"3: Add Shape",
+		"4: Delete Shape",
+		"5: Sort list",
+		"6: Clear list",
+		"7: Display Shape",
+		"8: Test 2D&3D",
+		
 };
+
 //menu Functions
 void ColorCycle(string text, int numCycles, int cycleSpeed);
 void ClearLine(int len);
 template<class T>
 void ErrorCheckLoop(T& userInput);
+//search & sort Functions
 template<class T>
-int binarySearch2D(float targetArea, const std::vector<T*>& vec);
+int binarySearch2D(float targetArea, const vector<T*>& vec);
 template<class T>
-int binarySearch3D(float targetArea, const std::vector<T*>& vec);
+int binarySearch3D(float targetArea, const vector<T*>& vec);
 void insertionSort(vector<Shape2D*>& vec);
 void insertionSort(vector<Shape3D*>& vec);
 using namespace std;
+
 int main()
 {
 
@@ -67,10 +72,11 @@ int main()
 	vector<Shape3D*> shapes3D;
 	vector<Shape2D*> test2d;
 	vector<Shape3D*> test3d;
+	vector<Shape*> testMixed;
 	bool menu = true;
 	//Welcome Message
 	cout << RED;
-	cout << "					    (Press Any Character to Skip)\n";
+	cout << "						 (Press Enter to Skip)\n";
 	string title = "                                 Joseph Somogie Project: Inheritance & polymorphism";
 	while(!_kbhit())
 	ColorCycle(title, 1, 100);
@@ -85,9 +91,12 @@ int main()
 	float select = 0;
 	while (menu)
 	{
+		select = -1;
+		command = '0';
+		optionIndex = 0;
 
 		
-
+		
 			system("cls");
 
 			cout << RED;
@@ -95,15 +104,15 @@ int main()
 			cout << YELLOW;
 			cout << "******PRESS NUM KEY FOR DESIRED COMMAND*******\n";
 			cout << BLUE;
-			cout << "1: Add Shape" << endl;
-			cout << "2: Delete Shape" << endl;
-			cout << "3: Clear List" << endl;
-			cout << "4: Sort list" << endl;
-			cout << "5: Search shapes" << endl;
-			cout << "6: Display Shape" << endl;
-			cout << "7: Test 2D" << endl;
-			cout << "8: Test 3D" << endl;
-			cout << "9: Test Mixed " << endl;
+			//cout << "1: Add Shape" << endl;
+			cout << "1: Test 2D" << endl;
+			cout << "2: Test 3D" << endl;
+			cout << "3: Add Shape" << endl;
+			cout << "4: Delete Shape" << endl;
+			cout << "5: Sort List" << endl;
+			cout << "6: Clear List" << endl;
+			cout << "7: Display Shape" << endl;
+			cout << "8: Test 2D&3D" << endl;
 			cout << "0: Exit Program" << endl;
 			cout << YELLOW;
 			cout << "*********************************************\n";
@@ -132,14 +141,140 @@ int main()
 
 			switch (command)
 			{
-			case '0':
+			case '0': //Exit Program
 				cout << CYAN;
 				cout << "THANKS!\n";
 				return 0;
 
 				break;
-			case '1':
+			case '1': //Test 2D
+				ColorCycle("TEST 2D", 1, 100);
+				if (test2d.size() == 0) {
+					test2d.push_back(new Square);
+					test2d.push_back(new Square(1.1));
+					test2d.push_back(new RectangleX);
+					test2d.push_back(new RectangleX(2.2, 3.3));
+					test2d.push_back(new Triangle());
+					test2d.push_back(new Triangle(4.4, 5.5));
+					test2d.push_back(new Circle());
+					test2d.push_back(new Circle(6.6));
+					test2d.push_back(new EllipseX());
+					test2d.push_back(new EllipseX(7.7, 8.8));
+					test2d.push_back(new Trapezoid());
+					test2d.push_back(new Trapezoid(1, 2, 3));
+					test2d.push_back(new Sector());
+					test2d.push_back(new Sector(5, 6));
+				}
+				for (auto Shape2D : test2d) {
+					std::this_thread::sleep_for(std::chrono::milliseconds(200));
+					Shape2D->Display();
+					cout << '\n';
+				}
+				cout << RED;
+				cout << "(Press enter to sort)";
+				_getch();
+				ClearLine(26);
+				ColorCycle("Sorting...", 1, 100);
+				cout << "-------------------\n";
+				insertionSort(test2d);
+				for (auto Shape2D : test2d) {
+					std::this_thread::sleep_for(std::chrono::milliseconds(200));
+					Shape2D->Display();
+					cout << '\n';
+				}
+				cout << "(Press enter to find min/max)";
+				_getch();
+				std::this_thread::sleep_for(std::chrono::milliseconds(200));
+				cout << "\nMIN\n";
+				test2d[0]->Display();
+				cout << '\n';
+				std::this_thread::sleep_for(std::chrono::milliseconds(200));
+				cout << "MAX\n";
+				test2d[test2d.size() - 1]->Display();
+				cout << '\n';
+				cout << "(Press enter to search by area)";
+				_getch();
+				ClearStream();
+				cout << "Enter area: ";
+				ErrorCheckLoop(select);
+				optionIndex = binarySearch2D(select, test2d);
+				cout << "\n Closest area shape was index #" << optionIndex << endl;
+				test2d[optionIndex]->Display();
+
+				cout << "(Press enter to scale this shape)";
+				_getch();
+				cout << "Enter Scalar: ";
+				ErrorCheckLoop(select);
+				test2d[optionIndex]->Scale(select);
+				cout << "\n Scaled Shape\n";
+				test2d[optionIndex]->Display();
+
+				cout << RED << "\nEND OF DEMO" << endl;
+				_getch();
 				
+				
+				break;
+			case '2': //Test 3D
+				ColorCycle("TEST 3D", 1, 100);
+				if (test3d.size() == 0) {
+					test3d.push_back(new TriangularPyramid());
+					test3d.push_back(new TriangularPyramid(3, 4, 5));
+					test3d.push_back(new RectangularPyramid());
+					test3d.push_back(new RectangularPyramid(5, 6, 7));
+					test3d.push_back(new Cylinder());
+					test3d.push_back(new Cylinder(3, 6));
+					test3d.push_back(new Sphere());
+					test3d.push_back(new Sphere(9));
+				}
+				for (auto Shape3D : test3d) {
+					std::this_thread::sleep_for(std::chrono::milliseconds(200));
+					Shape3D->Display();
+					cout << '\n';
+				}
+				cout << RED;
+				cout << "(Press enter to sort)";
+				_getch();
+				ClearLine(26);
+				ColorCycle("Sorting...", 1, 100);
+				cout << "-------------------\n";
+				insertionSort(test3d);
+				for (auto Shape3D : test3d) {
+					std::this_thread::sleep_for(std::chrono::milliseconds(200));
+					Shape3D->Display();
+					cout << '\n';
+				}
+				cout << "(Press enter to find min/max)";
+				_getch();
+				std::this_thread::sleep_for(std::chrono::milliseconds(200));
+				cout << "\nMIN\n";
+				test3d[0]->Display();
+				cout << '\n';
+				std::this_thread::sleep_for(std::chrono::milliseconds(200));
+				cout << "MAX\n";
+				test3d[test3d.size() - 1]->Display();
+				cout << '\n';
+				cout << "(Press enter to search by Volume)";
+				_getch();
+				ClearStream();
+				cout << "Enter volume: ";
+				ErrorCheckLoop(select);
+				optionIndex = binarySearch3D(select, test3d);
+				cout << "\n Closest volume shape was index #" << optionIndex << endl;
+				test3d[optionIndex]->Display();
+
+				cout << "(Press enter to scale this shape)";
+				_getch();
+				cout << "Enter Scalar: ";
+				ErrorCheckLoop(select);
+				test3d[optionIndex]->Scale(select);
+				cout << "\n Scaled Shape\n";
+				test3d[optionIndex]->Display();
+
+				cout << RED << "\nEND OF DEMO" << endl;
+				_getch();
+				
+				break;
+			case '3': //Add Shape
 				cout << CYAN;
 				cout << "PRESS: (1) 2D Shape (2) 3D Shape";
 				 select = _getch() - '0';
@@ -155,7 +290,7 @@ int main()
 					cout << "Trapezoid: 6\n";
 					cout << "Sector: 7\n";
 					select = _getch() - '0';
-					
+
 					if (select == 1) {
 						float s;
 						cout << BLUE;
@@ -236,7 +371,7 @@ int main()
 						float b, h, hP;
 						cout << GREEN;
 						cout << "Enter Triangle Base: ";
-						
+
 						ErrorCheckLoop(b);
 						cout << "\nEnter Triangle Height: ";
 						ErrorCheckLoop(h);
@@ -268,7 +403,7 @@ int main()
 						cout << "Enter Cylinder Raidus: ";
 						ErrorCheckLoop(r);
 						if (r != -1) {
-							
+
 							cout << "Enter Cylinder Height: ";
 
 							ErrorCheckLoop(h);
@@ -281,74 +416,97 @@ int main()
 						float r;
 						cout << GREEN;
 						cout << "Enter Sphere Radius: ";
-						
+
 						ErrorCheckLoop(r);
 						if(r!=-1)
 						shapes.push_back(new Sphere(r));
 						shapes3D.push_back(new Sphere(r));
-					}	
+					}
 				}
+
 				break;
-			case '2':
-				cout << CYAN;
-				cout << '\n';
-				cout << "Enter index of shape: ";
+			case '4': //delete Shape
+				cout << "PRESS: (1) 2D Shape (2) 3D Shape";
 				select = _getch() - '0';
-				while (select < 0 || select >= shapes.size()) {
-					cout << RED;
-					cout << " ERROR! INVALID INDEX";
-					std::this_thread::sleep_for(std::chrono::seconds(2));
-					ClearLine(21);
-					select = _getch() - '0';
+				cout << '\n';
+				if (select == 1 && !shapes2D.empty()) {
+					cout << "Enter index of shape: ";
+					ErrorCheckLoop(select);
+					while (select < 0 || select >= shapes2D.size()) {
+						cout << RED;
+						cout << " ERROR! INVALID INDEX";
+						ErrorCheckLoop(select);
+
+					}
+					delete shapes2D[select];
+					shapes2D.erase(shapes2D.begin() + select);
+					cout << "Shape Deleted" << endl;
 
 				}
-				delete shapes[select];
-				shapes.erase(shapes.begin() + select);
-				cout << "Shape Deleted" << endl;
-				break;
-			case '3':
-				cout << RED;
-				msg = "Clearing List...";
-				ColorCycle(msg, 2, 100);
-				for (auto& shape : shapes) {
-					delete shape;
-				}
-				shapes.clear();
-				ClearLine(msg.length());
-				cout << CYAN;
-				cout << "Cleared!";
+				if (select == 2 && !shapes3D.empty()) {
+					cout << "Enter index of shape: ";
+					ErrorCheckLoop(select);
+					while (select < 0 || select >= shapes3D.size()) {
+						cout << RED;
+						cout << " ERROR! INVALID INDEX";
+						ErrorCheckLoop(select);
 
+					}
+					delete shapes3D[select];
+					shapes3D.erase(shapes3D.begin() + select);
+					cout << "Shape Deleted" << endl;
+
+				}
+				else ColorCycle("Cancelling...", 1, 100);
+
+			
 				break;
-			case '4':
+			case '5': //Sort Shapes
 				cout << RED;
 				msg = "Sorting...";
 				ColorCycle(msg, 2, 100);
 				insertionSort(shapes2D);
-				break;
-			case '5':
+				insertionSort(shapes3D);
 				cout << CYAN;
+				
+				break;
+			case '6': //Clear List
 				cout << "PRESS: (1) 2D Shape (2) 3D Shape";
 				select = _getch() - '0';
+				cout << '\n';
+
+				cout << RED;
+				msg = "Clearing List...";
+				ColorCycle(msg, 2, 100);
+				if (select == 1) {
+					for (auto& Shape2D : shapes2D) {
+						delete Shape2D;
+					}
+					shapes2D.clear();
+				}
+				if (select == 2) {
+					for (auto& Shape3D : shapes3D) {
+						delete Shape3D;
+					}
+					shapes3D.clear();
+				}
+					ClearLine(msg.length()); 
+					cout << CYAN;
+					cout << "Cleared!";
+	
+					ClearStream();
 				
-				if (select == 1)
-				{
-					cout << "Enter Area: ";
-					ErrorCheckLoop(select);
-					//binS(select, shapes, 0, shapes.size() - 1);
-				}
-				if (select == 2)
-				{
-					//binS(select, shapes, 0, shapes.size() - 1);
-				}
 				break;
-			case '6':
+			case '7': //Display Shape
+				
+
 				cout << CYAN;
 				cout << "PRESS: (1) 2D Shape (2) 3D Shape";
 				select = _getch() - '0';
 				cout << '\n';
 				if (select == 1) {
-					
-					
+
+
 					for (auto Shape2D : shapes2D) {
 
 						Shape2D->Display();
@@ -357,149 +515,83 @@ int main()
 					}
 				}
 				else if (select == 2) {
-				
+
 					for (auto Shape3D : shapes3D) {
 						Shape3D->Display();
 						cout << '\n';
 					}
 				}
-					cout << "(Press enter to continue)";
-					_getch();
-					ClearStream();
+				cout << "(Press enter to continue)";
+				_getch();
+				ClearStream();
 				
+
 				break;
-			case '7':
-				ColorCycle("TEST 2D", 1, 100);
-				if (test2d.size() == 0) {
-					test2d.push_back(new Square);
-					test2d.push_back(new Square(1.1));
-					test2d.push_back(new RectangleX);
-					test2d.push_back(new RectangleX(2.2, 3.3));
-					test2d.push_back(new Triangle());
-					test2d.push_back(new Triangle(4.4, 5.5));
-					test2d.push_back(new Circle());
-					test2d.push_back(new Circle(6.6));
-					test2d.push_back(new EllipseX());
-					test2d.push_back(new EllipseX(7.7, 8.8));
-					test2d.push_back(new Trapezoid());
-					test2d.push_back(new Trapezoid(1, 2, 3));
-					test2d.push_back(new Sector());
-					test2d.push_back(new Sector(5, 6));
+			case '8': //Test Mixed
+				ColorCycle("TEST MIXED SHAPES", 1, 100);
+				if (testMixed.size() == 0) {
+					testMixed.push_back(new TriangularPyramid());
+					testMixed.push_back(new Square(5));
+					testMixed.push_back(new TriangularPyramid(3, 4, 5));
+					testMixed.push_back(new RectangleX(3, 5.5));
+					testMixed.push_back(new RectangularPyramid());
+					testMixed.push_back(new RectangularPyramid(5, 6, 7));
+					testMixed.push_back(new Cylinder());
+					testMixed.push_back(new Cylinder(3, 6));
+					testMixed.push_back(new Sphere());
+					testMixed.push_back(new Sphere(9));
 				}
-				for (auto Shape2D : test2d) {
+				for (auto Shape : testMixed) {
 					std::this_thread::sleep_for(std::chrono::milliseconds(200));
-					Shape2D->Display();
+					Shape->Display();
 					cout << '\n';
 				}
 				cout << RED;
-				cout << "(Press enter to sort)";
+				cout << "(Press enter to scale)";
 				_getch();
-				ClearLine(26);
-				ColorCycle("Sorting...", 1, 100);
-				cout << "-------------------\n";
-				insertionSort(test2d);
-				for (auto Shape2D : test2d) {
-					std::this_thread::sleep_for(std::chrono::milliseconds(200));
-					Shape2D->Display();
-					cout << '\n';
+				cout << "Enter shape index to scale: ";
+				ErrorCheckLoop(optionIndex);
+				while (optionIndex >= testMixed.size() || optionIndex < 0) {
+					cout << "Enter valid index 0-" << testMixed.size() - 1 << " :";
+					ErrorCheckLoop(optionIndex);
 				}
-				cout << "(Press enter to find min/max)";
-				_getch();
-				std::this_thread::sleep_for(std::chrono::milliseconds(200));
-				cout << "\nMIN\n";
-				test2d[0]->Display();
-				cout << '\n';
-				std::this_thread::sleep_for(std::chrono::milliseconds(200));
-				cout << "MAX\n";
-				test2d[test2d.size() - 1]->Display();
-				cout << '\n';
-				cout << "(Press enter to search by area)";
-				_getch();
-				ClearStream();
-				cout << "Enter area: ";
+				testMixed[optionIndex]->Display();
+				cout << "\nEnter Scalar: ";
 				ErrorCheckLoop(select);
-				optionIndex = binarySearch2D(select, test2d);
-				cout << "\n Closest area shape was index #" << optionIndex << endl;
-				test2d[optionIndex]->Display();
-				
-				cout << "(Press enter to scale this shape)";
-				_getch();
-				cout << "Enter Scalar: ";
-				ErrorCheckLoop(select);
-				test2d[optionIndex]->Scale(select);
+				testMixed[optionIndex]->Scale(select);
 				cout << "\n Scaled Shape\n";
-				test2d[optionIndex]->Display();
-
-				cout << RED << "\nEND OF DEMO" << endl;
-				_getch();
-
-				break;
-			case '8':
-				ColorCycle("TEST 3D", 1, 100);
-				if (test3d.size() == 0) {
-					test3d.push_back(new TriangularPyramid());
-					test3d.push_back(new TriangularPyramid(3, 4, 5));
-					test3d.push_back(new RectangularPyramid());
-					test3d.push_back(new RectangularPyramid(5, 6, 7));
-					test3d.push_back(new Cylinder());
-					test3d.push_back(new Cylinder(3, 6));
-					test3d.push_back(new Sphere());
-					test3d.push_back(new Sphere(9));
-				}
-				for (auto Shape3D : test3d) {
-					std::this_thread::sleep_for(std::chrono::milliseconds(200));
-					Shape3D->Display();
-					cout << '\n';
-				}
-				cout << RED;
-				cout << "(Press enter to sort)";
-				_getch();
-				ClearLine(26);
-				ColorCycle("Sorting...", 1, 100);
-				cout << "-------------------\n";
-				insertionSort(test3d);
-				for (auto Shape3D : test3d) {
-					std::this_thread::sleep_for(std::chrono::milliseconds(200));
-					Shape3D->Display();
-					cout << '\n';
-				}
-				cout << "(Press enter to find min/max)";
-				_getch();
-				std::this_thread::sleep_for(std::chrono::milliseconds(200));
-				cout << "\nMIN\n";
-				test3d[0]->Display();
-				cout << '\n';
-				std::this_thread::sleep_for(std::chrono::milliseconds(200));
-				cout << "MAX\n";
-				test3d[test3d.size() - 1]->Display();
-				cout << '\n';
-				cout << "(Press enter to search by Volume)";
-				_getch();
-				ClearStream();
-				cout << "Enter volume: ";
-				ErrorCheckLoop(select);
-				optionIndex = binarySearch3D(select, test3d);
-				cout << "\n Closest volume shape was index #" << optionIndex << endl;
-				test3d[optionIndex]->Display();
-
-				cout << "(Press enter to scale this shape)";
-				_getch();
-				cout << "Enter Scalar: ";
-				ErrorCheckLoop(select);
-				test3d[optionIndex]->Scale(select);
-				cout << "\n Scaled Shape\n";
-				test3d[optionIndex]->Display();
+				testMixed[optionIndex]->Display();
 
 				cout << RED << "\nEND OF DEMO" << endl;
 				_getch();
 				break;
-			case '9':
+			case '9': //Search Shapes (broken)
+				cout << "PRESS: (1) 2D Shape (2) 3D Shape";
+				select = _getch() - '0';
+
+				if (select == 1)
+				{
+					cout << "Enter Area: ";
+					ErrorCheckLoop(select);
+					optionIndex = binarySearch2D(select, shapes2D);
+					cout << "\nShape is index #" << optionIndex << endl;
+					shapes2D[optionIndex]->Display();
+				}
+				if (select == 2)
+				{
+					cout << "Enter Volume: ";
+					ErrorCheckLoop(select);
+					optionIndex = binarySearch3D(select, shapes3D);
+					cout << "\nShape is index #" << optionIndex << endl;
+					shapes3D[optionIndex]->Display();
+				}
 				break;
 
 
 			}
 
 			std::this_thread::sleep_for(std::chrono::milliseconds(900));
+			ClearStream();
 			system("cls");
 		
 	}
